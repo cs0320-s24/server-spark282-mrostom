dbded31ef2124a8e1d707b6b6fca7ed18caf65a5
“MichaelRostom2” <michael_rostom@brown.edu>
2024-02-16 11:12:08 -0500

    refact: javadoc BroadbandHandler

--- src/main/java/edu/brown/cs/student/main/server/datasources/APIDataSourceInterface.java
+++ src/main/java/edu/brown/cs/student/main/server/datasources/APIDataSourceInterface.java
@@ -5,7 +5,10 @@ import edu.brown.cs.student.main.server.handlers.DatasourceException;
 import java.io.IOException;
 import java.util.List;
 
+/**
+ * Interface for an Api data source. All classes must have a parser and a getter.
+ */
 public interface APIDataSourceInterface {
   List<List<String>> getData(String State, String County)
-      throws IOException, FactoryFailureException, DatasourceException;
+      throws DatasourceException;
 }
